4124e76dc4fe2a0a93e03c201df284c28794a306
MamelWorks <mamelworks@users.noreply.github.com>
2025-12-29 05:26:07 +0100

    Use javaw to launch client without CMD window

--- Play.bat
+++ Play.bat
@@ -1,6 +1,6 @@
 @echo off
-if not exist "jdk-21.0.6+7\bin\java.exe" (
-    echo ERROR: Java 21 not found at jdk-21.0.6+7\bin\java.exe
+if not exist "jdk-21.0.6+7\bin\javaw.exe" (
+    echo ERROR: Java 21 not found at jdk-21.0.6+7\bin\javaw.exe
     echo Please make sure JDK is installed in the correct location.
     pause
     exit /b 1
@@ -15,8 +15,4 @@ if not exist "bin\hafen.jar" (
 
 echo Starting Hurricane client...
 cd bin
-..\jdk-21.0.6+7\bin\java -Dsun.java2d.uiScale.enabled=false -Dsun.java2d.win.uiScaleX=1.0 -Dsun.java2d.win.uiScaleY=1.0 -Xss8m -Xms1024m -Xmx4096m --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED -DrunningThroughSteam=true -DrunningThroughDiscord=true -jar hafen.jar > ..\client.log 2>&1
-
-REM Log exit code and close automatically
-echo Client exited with code: %ERRORLEVEL% >> ..\client.log
-exit
+start "Hurricane Client" ..\jdk-21.0.6+7\bin\javaw -Dsun.java2d.uiScale.enabled=false -Dsun.java2d.win.uiScaleX=1.0 -Dsun.java2d.win.uiScaleY=1.0 -Xss8m -Xms1024m -Xmx4096m --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED -DrunningThroughSteam=true -DrunningThroughDiscord=true -jar hafen.jar
--- Play_MultiInstance.bat
+++ Play_MultiInstance.bat
@@ -2,8 +2,8 @@
 set INSTANCE=%1
 if "%INSTANCE%"=="" set INSTANCE=1
 
-if not exist "jdk-21.0.6+7\bin\java.exe" (
-    echo ERROR: Java 21 not found at jdk-21.0.6+7\bin\java.exe
+if not exist "jdk-21.0.6+7\bin\javaw.exe" (
+    echo ERROR: Java 21 not found at jdk-21.0.6+7\bin\javaw.exe
     echo Please make sure JDK is installed in the correct location.
     pause
     exit /b 1
@@ -18,4 +18,4 @@ if not exist "bin\hafen.jar" (
 
 echo Starting Hurricane client instance %INSTANCE%...
 cd bin
-start "Hurricane Instance %INSTANCE%" ..\jdk-21.0.6+7\bin\java -Dsun.java2d.uiScale.enabled=false -Dsun.java2d.win.uiScaleX=1.0 -Dsun.java2d.win.uiScaleY=1.0 -Xss8m -Xms1024m -Xmx4096m --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED -DrunningThroughDiscord=true -jar hafen.jar > ..\client_%INSTANCE%.log 2>&1
+start "Hurricane Instance %INSTANCE%" ..\jdk-21.0.6+7\bin\javaw -Dsun.java2d.uiScale.enabled=false -Dsun.java2d.win.uiScaleX=1.0 -Dsun.java2d.win.uiScaleY=1.0 -Xss8m -Xms1024m -Xmx4096m --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED -DrunningThroughDiscord=true -jar hafen.jar
