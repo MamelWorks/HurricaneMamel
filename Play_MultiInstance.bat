1887c1f2d76dc1703f221fc96b858e02ddd3960d
MamelWorks <mamelworks@users.noreply.github.com>
2025-12-29 03:41:55 +0100

    Add multi-instance launcher without Steam integration

---
+++ Play_MultiInstance.bat
@@ -0,0 +1,21 @@
+@echo off
+set INSTANCE=%1
+if "%INSTANCE%"=="" set INSTANCE=1
+
+if not exist "jdk-21.0.6+7\bin\java.exe" (
+    echo ERROR: Java 21 not found at jdk-21.0.6+7\bin\java.exe
+    echo Please make sure JDK is installed in the correct location.
+    pause
+    exit /b 1
+)
+
+if not exist "bin\hafen.jar" (
+    echo ERROR: bin\hafen.jar not found!
+    echo Please run the build first.
+    pause
+    exit /b 1
+)
+
+echo Starting Hurricane client instance %INSTANCE%...
+cd bin
+start "Hurricane Instance %INSTANCE%" ..\jdk-21.0.6+7\bin\java -Dsun.java2d.uiScale.enabled=false -Dsun.java2d.win.uiScaleX=1.0 -Dsun.java2d.win.uiScaleY=1.0 -Xss8m -Xms1024m -Xmx4096m --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED -DrunningThroughDiscord=true -jar hafen.jar > ..\client_%INSTANCE%.log 2>&1
